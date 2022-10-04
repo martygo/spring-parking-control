e81af080bbeeec82ad86eac79dc21fe4ed38929f
Martins Gouveia <gouveiamartins6@gmail.com>
2022-10-04 18:27:08 +0100

    📝 add description and title on documentation

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -2,12 +2,30 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("ParkingSpot API")
+						.description("API that simulates the management of parking spaces")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation()
+						.description("ParkingSpot Wiki Documentation")
+						.url("https://parkingspot.wiki.github.org/docs"));
 	}
 
 }
